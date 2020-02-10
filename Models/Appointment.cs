258d6835b644255452636b9b4bc9b6b7fbef49f6
Sydney Kovach <sk072215@ohio.edu>
2020-02-10 18:47:53 -0500

    appointment

---
+++ Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SD1_sk072215_MIS4200.Models
+{
+    public class Appointment
+    {
+
+        [Key]
+        public int appointmentID { get; set; }
+        public string description { get; set; }
+        public DateTime appointmentDate { get; set; }
+        public ICollection<AppointmentDetail> AppointmentDetail { get; set; }
+        public int patientId { get; set; }
+        public virtual Patient Patient { get; set; }
+    }
+}
