c520e89f84103fddaa12450bae8513096bc9367c
Sydney Kovach <sk072215@ohio.edu>
2020-02-10 18:32:21 -0500

    Patients

---
+++ Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SD1_sk072215_MIS4200.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime patientSince { get; set; }
+
+        public ICollection<Appointment> Appointment { get; set; }
+    }
+}
