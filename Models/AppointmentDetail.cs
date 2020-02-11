c2903bb3c79461dcc556c77957c5b14a9cc0603d
Sydney Kovach <sk072215@ohio.edu>
2020-02-10 19:17:38 -0500

    appointmentdetail

---
+++ Models/AppointmentDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SD1_sk072215_MIS4200.Models
+{
+    public class AppointmentDetail
+    {
+        public int appointmentDetailID { get; set; }
+        public int qtyProcedure { get; set; }
+        public decimal price { get; set; }
+       
+
+        // link appoitmentdetail to appointment:
+        public int AppointmentID { get; set; }
+        public virtual Appointment Appointment { get; set; }
+
+        // link appoitmentdetail to procedure
+        public int procedureId { get; set; }
+        public virtual Procedure Procedure { get; set; }
+    }
+}
