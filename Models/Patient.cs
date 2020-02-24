db005b66413c5af1f921c8a6d5ab1d136c8ed219
Sydney Kovach <sk072215@ohio.edu>
2020-02-24 15:38:37 -0500

    Patient Annotation

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -1,19 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SD1_sk072215_MIS4200.Models
 {
-    public class Patient
+    public partial class Patient
     {
-
+        public System.Guid SID { get; set; }
         public int PatientId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="Patient First Name Required")]
+        [StringLength(50)]
         public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Patient Last Name Required")]
+        [StringLength(50)]
         public string lastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Email Address is Required for contact")]
         public string email { get; set; }
+
+        [Display(Name = "Mobile Phone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$",
+            ErrorMessage ="Please type your phone number using the following formats (xxx) xxx-xxxx OR xxx-xxx-xxxx")]
         public string phone { get; set; }
-        public DateTime patientSince { get; set; }
+
+        [Display(Name = "Date of Registration")]
+        [DataType(DataType.Date)]
+        [DisplayFormat (DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> patientSince { get; set; }
 
         public ICollection<Appointment> Appointment { get; set; }
     }
