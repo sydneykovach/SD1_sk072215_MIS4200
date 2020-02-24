d1b0a803fb99cea933ff57cc24d0bf7f60625a08
Sydney Kovach <sk072215@ohio.edu>
2020-02-24 16:03:00 -0500

    appointment annotations

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace SD1_sk072215_MIS4200.Models
 {
     public class Appointment
     {
+        public System.Guid SID { get; set; }
 
         [Key]
         public int appointmentID { get; set; }
+
+        [Display (Name = "What type of Appointment? (consultation, surgery, follow up)")]
+        [Required]
         public string description { get; set; }
+
+        [Display(Name = "Appointment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime appointmentDate { get; set; }
         public ICollection<AppointmentDetail> AppointmentDetail { get; set; }
+
+        
         public int patientId { get; set; }
+       
         public virtual Patient Patient { get; set; }
     }
 }
